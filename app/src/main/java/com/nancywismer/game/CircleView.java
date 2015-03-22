569c5af7465074015a94f4193ceba4548941c9d0
nancydawiz <nancy.wismer@gmail.com>
2015-03-22 06:02:00 -0400

    disappearing circles

---
+++ app/src/main/java/com/nancywismer/game/CircleView.java
@@ -0,0 +1,14 @@
+package com.nancywismer.game;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Nancy on 3/22/2015.
+ */
+public class CircleView extends View {
+    public CircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
--- app/src/main/java/com/nancywismer/game/GameActivity.java
+++ app/src/main/java/com/nancywismer/game/GameActivity.java
@@ -1,62 +1,85 @@
 package com.nancywismer.game;
 
+import android.opengl.Visibility;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 
+import java.util.Random;
+
 
 public class GameActivity extends ActionBarActivity {
 
     private int mScore = 0;
-    private View mTopBar;
+    Animation up;
+    View[] cir = new View[15];
+    private int [] winners = new int[5];
+    private int loser;
+
+   /* View.OnClickListener mListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            cir[0].setBackgroundColor(0xff0000);
+            cir[12].setBackgroundColor(0xffff00);
+        }
+    };*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Random random = new Random();
+        loser = random.nextInt();
+        do
+            winners[0] = random.nextInt(14);
+        while (winners[0] == loser);
 
-        //Animation that makes the bubbles go up
-        final Animation up = new TranslateAnimation(0,0,0,-1050);
-        up.setDuration(2000);
-        up.setRepeatCount(-1);
-
-        final View circle = findViewById(R.id.circle);
-        circle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                circle.setVisibility(View.INVISIBLE);
+        for(int i = 1; i < 13; i ++){
+            winners[i] = random.nextInt(14);
+            while (winners[i] == winners[i-1] || winners[i] == loser){
+                winners[i] = random.nextInt();
             }
-        });
-        circle.setAnimation(up);
-
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_game, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
+        cir[0] = findViewById(R.id.circle1);
+        cir[1] = findViewById(R.id.circle2);
+        cir[2] = findViewById(R.id.circle3);
+        cir[3] = findViewById(R.id.circle4);
+        cir[4] = findViewById(R.id.circle5);
+        cir[5] = findViewById(R.id.circle6);
+        cir[6] = findViewById(R.id.circle7);
+        cir[7] = findViewById(R.id.circle8);
+        cir[8] = findViewById(R.id.circle9);
+        cir[9] = findViewById(R.id.circle10);
+        cir[10] = findViewById(R.id.circle11);
+        cir[11] = findViewById(R.id.circle12);
+        cir[12] = findViewById(R.id.circle13);
+        cir[13] = findViewById(R.id.circle14);
+        cir[14] = findViewById(R.id.circle15);
+
+
+        for (int i = 0; i < 15; i++) {
+            final int finalI = i;
+            cir[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (finalI == winners[0] || finalI == winners[1] || finalI == winners[2] || finalI == winners[3]) {
+                        cir[finalI].setBackgroundColor(0xff0000);
+                    }
+                }
+            });
+        }
     }
 }
+
+
